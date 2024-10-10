9f1cc4816f45983fc41c123a5e481d376261c370
Danila Dippel <erroraeto@gmail.com>
2024-10-10 21:05:13 +0300

    Huge remake

--- js/horizontal-scroll.js
+++ js/horizontal-scroll.js
@@ -1,4 +1,4 @@
-let content = document.querySelector('.main');
+let main = document.querySelector('.main');
 let pos = 0;
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -10,18 +10,18 @@ document.addEventListener('click', async function (e) {
     let childPos = [...node.children].indexOf(e.target);
 
     if (childPos != pos && childPos > pos) {
-        Array.from(content.children).forEach((elem) => elem.style.scale = '0.8');
+        main.style.transform = 'translateZ(-20rem)';
         await sleep(500);
-        content.style.translate = -100 + 'vw';
+        main.style.translate = -100 + 'vw';
         await sleep(700);
-        Array.from(content.children).forEach((elem) => elem.style.scale = '');
+        main.style.transform = '';
         pos += 1;
     } else if (childPos != pos && childPos < pos) {
-        Array.from(content.children).forEach((elem) => elem.style.scale = '0.8');
+        main.style.transform = 'translateZ(-20rem)';
         await sleep(500);
-        content.style.translate = '';
+        main.style.translate = '';
         await sleep(700);
-        Array.from(content.children).forEach((elem) => elem.style.scale = '');
+        main.style.transform = '';
         pos -= 1;
     }
 });
