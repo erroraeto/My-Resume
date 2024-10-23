658950cc0ea2da28e3527e4ca3f98ef33576610b
Danila Dippel <erroraeto@gmail.com>
2024-10-23 23:27:40 +0300

    global fix & footer done

---
+++ js/contact.js
@@ -0,0 +1,64 @@
+//Анимация глаза
+let x = +bounds.getAttribute('cx');
+let y = +bounds.getAttribute('cy');
+let dr = +bounds.getAttribute('r') - drag.getAttribute('r');
+
+document.addEventListener('mousemove', function(e) {
+    X0 = bounds.getBoundingClientRect().left + +bounds.getAttribute('r');
+    Y0 = bounds.getBoundingClientRect().top + +bounds.getAttribute('r');
+    let dy = Y0 - e.y;
+    let dx = X0 - e.x;
+    if (dx*dx + dy*dy < dr*dr) {
+        dx = e.offsetX;
+        dy = e.offsetY;
+    } else {
+        let a = Math.atan2(dy, dx);
+        dx = x - Math.cos(a)*dr;
+        dy = y - Math.sin(a)*dr;
+    } 
+    drag.setAttribute('transform', `translate(${dx},${dy})`);  
+});
+document.onmousedown = () => drag.setAttribute('r', `2.5`);
+document.onmouseup = () => drag.setAttribute('r', `4`);
+
+//Открытие контактов
+let footer = document.querySelector('.footer');
+let contacts = document.querySelector('.contacts');
+let opened = false;
+
+document.addEventListener('click', function (e) {
+    if (!e.target.closest('.contact_target')) return;
+
+    if (opened) {
+        footer.style.transform = 'translate3d(0, 12.8rem, 0)';
+        contacts.style.display = 'none';
+        opened = false;
+    } else {
+        footer.style.transform = '';
+        contacts.style.display = 'grid';
+        opened = true;
+    }
+});
+
+//Анимация наведения на контакты
+let icons = document.querySelectorAll('.icon');
+
+document.addEventListener('mouseover', function(e) {
+    if ((!e.target.closest('.icon')) || e.target.className == "icon") return;
+    
+    let targetIcon = e.target.closest('.icon');
+    targetIcon.style.transform = "translate3d(0, 0, 4rem)";
+
+    for (icon of icons) {
+        if (targetIcon == icon) continue;
+        icon.style.filter = "blur(1px)";
+    }
+    
+    targetIcon.addEventListener('mouseout', function() {
+        targetIcon.style.transform = "";
+        for (icon of icons) {
+            if (targetIcon == icon) continue;
+            icon.style.filter = "";
+        }
+    })
+});
