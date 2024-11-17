d43f87ae7dce021744a25b8ea7556497a1841d2d
Danila Dippel <erroraeto@gmail.com>
2024-11-17 12:58:11 +0300

    Penta remake

--- js/events.js
+++ js/events.js
@@ -78,6 +78,157 @@ document.addEventListener('mouseout', function(e) {
     if (e.target.classList[0] != "state") return;
     parametr.style.clipPath = "polygon(49.8% 75.51%, 64% 65%, 91.2% 59%, 73.8% 38%, 67.68% 20.58%, 49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%, 10.73% 58.3%, 35.59% 66.24%)"
 });
+
+
+
+
+!function(prt){
+    prt._getAbsoluteCTM = function(){
+        var owner = this.ownerSVGElement || this,
+            height = owner.height.baseVal.value,
+            width = owner.width.baseVal.value,
+            viewBoxRect = owner.viewBox.baseVal,
+            vHeight = viewBoxRect.height,
+            vWidth = viewBoxRect.width;
+        if(!vWidth || !vHeight){
+            return this.getCTM();
+        }
+        var sH = height/vHeight,
+            sW = width/vWidth,
+            matrix = owner.createSVGMatrix();
+        matrix.a = sW;
+        matrix.d = sH
+        var realCTM = this.getCTM().multiply(matrix.inverse());
+        realCTM.e = realCTM.e/sW + viewBoxRect.x;
+        realCTM.f = realCTM.f/sH + viewBoxRect.y;
+        return realCTM;
+    }
+}(SVGGraphicsElement.prototype);
+
+
+
+
+let test = document.querySelector("#TEST");
+let one = document.querySelector("#ONE");
+let two = document.querySelector("#TWO");
+let line = document.querySelector("#line0");
+let crc = document.querySelectorAll(".crc");
+let tst = document.querySelectorAll(".tst");
+let lne = document.querySelectorAll(".line");
+let pnta = document.querySelector("#pnta");
+let losta = document.querySelector("#losta");
+const map1 = new Map();
+
+document.addEventListener('DOMContentLoaded', async function() {
+    for (let i=0;i<crc.length;i++) {
+        map1.set(crc[i], tst[i]);
+    };
+    while(true) {
+        pnta.attributes.d.value = "M";
+        for (let i=0;i<map1.size;i++) {
+            // let m = losta.getTransformToElement(crc[i]);
+            let werty = losta.getScreenCTM().inverse().multiply( crc[i].getScreenCTM() );
+            let CTM = crc[i].getScreenCTM();
+            let asdfghj = crc[i].getBBox();
+            let s = crc[i].getCTM();
+            let {width,height} = crc[i].getBBox();
+            // let {x,y} = crc[i].getBoundingClientRect();
+            // x = x + width/2;
+            // y = y + height/2;
+            x = werty.e;
+            y = werty.f;
+            tst[i].setAttribute("cx", x);
+            tst[i].setAttribute("cy", y);
+            // pnta.attributes.d.value += `${x/2}, ${y/2} `;
+        };
+        for (let i=0;i<map1.size;i++) {
+            pnta.attributes.d.value += `${map1.get(crc[i]).getAttribute("cx")}, ${map1.get(crc[i]).getAttribute("cy")} `;
+            // let curr = i;
+            // lne[i].setAttribute("x1", map1.get(crc[i]).getAttribute("cx"));
+            // lne[i].setAttribute("y1", map1.get(crc[i]).getAttribute("cy"));
+            // curr = ++curr % map1.size;
+            // lne[i].setAttribute("x2", map1.get(crc[curr]).getAttribute("cx"));
+            // lne[i].setAttribute("y2", map1.get(crc[curr]).getAttribute("cy"));
+        };
+        pnta.attributes.d.value += "z";
+        await sleep(30);
+    };
+    function coordinateTransform(screenPoint, someSvgObject) {
+        var CTM = someSvgObject.getScreenCTM();
+        return screenPoint.matrixTransform( CTM.inverse() );
+    };
+    // one.cx.baseVal.value;
+    // one.cy.baseVal.value;
+    // one.lastElementChild;
+    // let m = one.getTransformToElement(saas);
+    // let asdsa = one.getAttributeNS(null, 'cx');
+    // let onePos = one.getBoundingClientRect();
+    // let bboxGroup = one.getBBox();
+    // let a = one.getScreenCTM();
+    // let s = one.getCTM();
+    // let inverse = a.inverse();
+    // let inverse = a.getScreenCoords();
+    // var CTM = one.getScreenCTM();
+    // line.x1.baseVal.value = (one.clientLeft - CTM.e) / CTM.a;
+    // line.y1.baseVal.value = (one.clientTop - CTM.f) / CTM.d;
+    // line.x1.baseVal.value = s.e + 170;
+    // line.y1.baseVal.value = -s.f;
+    // line.x1.baseVal.value = -inverse.e;
+    // line.y1.baseVal.value = -s.f;
+    // line.x1.baseVal.value = one.cx.baseVal.value;
+    // line.y1.baseVal.value = one.cy.baseVal.value;
+    // line.x2.baseVal.value;
+    // line.y2.baseVal.value;
+    // let das = one._getAbsoluteCTM();
+});
+
+
+// let flag = 0;
+// // svg = document.querySelector('#ONE');
+// let losta = document.querySelector('#losta');
+// // $svg = $(svg);
+
+// let pause = function() {
+//     let fasd = losta.animate();
+//     // fasd.progress();
+//     // let rect = draw.rect(100, 100);
+//     // let runner = rect.animate();
+//     losta.progress();
+// };
+
+
+// function distance(start: Point, end: Point) {
+//     const dx = to.x - from.x
+//     const dy = to.y - from.y
+
+//     return Math.sqrt(dx * dx + dy * dy)
+// }
+
+// function calculatePath(start: Point, end: Point) {
+//     const center = {
+//         x: (start.x + end.x) / 2,
+//         y: (start.y + end.y) / 2,
+//     }
+
+//     const controlPoint = {
+//         x: start.x + Math.min(
+//             distance(start, end),
+//             Math.abs(end.y - start.y) / 2,
+//             150
+//         ),
+//         y: start.y,
+//     }
+
+//     return `M ${start.x},${start.y} Q ${controlPoint.x}, ${controlPoint.y} ${center.x},${center.y} T ${end.x},${end.y}`
+// }
+
+
+
+
+
+
+
+
 //Анимация скиллов
 document.addEventListener('DOMContentLoaded', async function(e) {
     R = {
