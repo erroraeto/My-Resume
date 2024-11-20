b4d00b3e5fb89d1daea838c82c1fdc5badeb6f3a
Danila Dippel <erroraeto@gmail.com>
2024-11-20 18:24:07 +0300

    Penta valid make

--- js/events.js
+++ js/events.js
@@ -41,7 +41,8 @@ function sleep(ms) {
 //Прокрутка к основному фрейму
 window.onload = function() {
     document.all[0].style.scrollBehavior = "auto";
-    home.scrollIntoView({behavior: "auto"});
+    // home.scrollIntoView({behavior: "auto"});
+    skill.scrollIntoView({behavior: "auto"});
     document.all[0].style.scrollBehavior = "";
 }
 
@@ -52,223 +53,103 @@ document.addEventListener('click', async function (e) {
     containerChoosed.scrollIntoView({ behavior: 'smooth' });
 });
 
-//Выбор скилла
-let parametr = document.querySelector(".parametr");
-let states = document.querySelectorAll(".state");
 
-document.addEventListener('mouseover', function(e) {
-    if (e.target.className.baseVal != "state") return;
-
-    if (e.target == states[0]) {
-        // parametr.style.clipPath = "polygon(49.8% 75.51%, 64% 65%, 91.2% 59%, 73.8% 38%, 67.68% 20.58%, 49.9% 17.12%, 22.2% 4.5%, 20% 36.2%, 10.73% 58.3%, 35.59% 66.24%)"
-        parametr.style.clipPath = parametr.style.clipPath.replace(/49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%/g, '49.9% 17.12%, 22.2% 4.5%, 20% 36.2%');
-        // parametr.style.clipPath.replace(/49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%/g, '49.9% 17.12%, 22.2% 4.5%, 20% 36.2%')
-    } else if (e.target == states[1]) {
-        parametr.style.clipPath = "polygon(49.8% 75.51%, 64% 65%, 91.2% 59%, 73.8% 38%, 67.68% 20.58%, 49.8% 21.25%, 29.2% 16.45%, 22% 36.8%, 4.8% 60.3%, 34.8% 67%)"
-    } else if (e.target == states[2]) {
-        parametr.style.clipPath = "polygon(49.9% 94.7%, 68.3% 72%, 91.2% 59%, 73.8% 38%, 67.68% 20.58%, 49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%, 10.73% 58.3%, 31.5% 72%)"
-    } else if (e.target == states[3]) {
-        parametr.style.clipPath = "polygon(49.8% 75.51%, 65% 67.25%, 95% 60.3%, 75.94% 37.5%, 67.68% 20.58%, 49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%, 10.73% 58.3%, 35.59% 66.24%)"
-    } else if (e.target == states[4]) {
-        parametr.style.clipPath = "polygon(49.8% 75.51%, 64% 65%, 91.2% 59%, 78% 36.8%, 77.68% 4.5%, 49.9% 14.50%, 29.2% 16.45%, 24.69% 37.8%, 10.73% 58.3%, 35.59% 66.24%)"
-    }
-});
-
-document.addEventListener('mouseout', function(e) {
-    if (e.target.classList[0] != "state") return;
-    parametr.style.clipPath = "polygon(49.8% 75.51%, 64% 65%, 91.2% 59%, 73.8% 38%, 67.68% 20.58%, 49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%, 10.73% 58.3%, 35.59% 66.24%)"
-});
-
-
-
-
-!function(prt){
-    prt._getAbsoluteCTM = function(){
-        var owner = this.ownerSVGElement || this,
-            height = owner.height.baseVal.value,
-            width = owner.width.baseVal.value,
-            viewBoxRect = owner.viewBox.baseVal,
-            vHeight = viewBoxRect.height,
-            vWidth = viewBoxRect.width;
-        if(!vWidth || !vHeight){
-            return this.getCTM();
-        }
-        var sH = height/vHeight,
-            sW = width/vWidth,
-            matrix = owner.createSVGMatrix();
-        matrix.a = sW;
-        matrix.d = sH
-        var realCTM = this.getCTM().multiply(matrix.inverse());
-        realCTM.e = realCTM.e/sW + viewBoxRect.x;
-        realCTM.f = realCTM.f/sH + viewBoxRect.y;
-        return realCTM;
-    }
-}(SVGGraphicsElement.prototype);
-
-
-
-
-let test = document.querySelector("#TEST");
-let one = document.querySelector("#ONE");
-let two = document.querySelector("#TWO");
-let line = document.querySelector("#line0");
 let crc = document.querySelectorAll(".crc");
-let tst = document.querySelectorAll(".tst");
-let lne = document.querySelectorAll(".line");
 let pnta = document.querySelector("#pnta");
 let losta = document.querySelector("#losta");
 const map1 = new Map();
+const crcCord = new Map();
 
 document.addEventListener('DOMContentLoaded', async function() {
-    for (let i=0;i<crc.length;i++) {
-        map1.set(crc[i], tst[i]);
-    };
     while(true) {
         pnta.attributes.d.value = "M";
-        for (let i=0;i<map1.size;i++) {
-            // let m = losta.getTransformToElement(crc[i]);
-            let werty = losta.getScreenCTM().inverse().multiply( crc[i].getScreenCTM() );
-            let CTM = crc[i].getScreenCTM();
-            let asdfghj = crc[i].getBBox();
-            let s = crc[i].getCTM();
-            let {width,height} = crc[i].getBBox();
-            // let {x,y} = crc[i].getBoundingClientRect();
-            // x = x + width/2;
-            // y = y + height/2;
-            x = werty.e;
-            y = werty.f;
-            tst[i].setAttribute("cx", x);
-            tst[i].setAttribute("cy", y);
-            // pnta.attributes.d.value += `${x/2}, ${y/2} `;
+        crcCord.clear();
+        for (let i=0;i<crc.length;i++) {
+            let crcPos = losta.getScreenCTM().inverse().multiply( crc[i].getScreenCTM() );
+            x = crcPos.e;
+            y = crcPos.f;
+            crcCord.set(x, y);
         };
-        for (let i=0;i<map1.size;i++) {
-            pnta.attributes.d.value += `${map1.get(crc[i]).getAttribute("cx")}, ${map1.get(crc[i]).getAttribute("cy")} `;
-            // let curr = i;
-            // lne[i].setAttribute("x1", map1.get(crc[i]).getAttribute("cx"));
-            // lne[i].setAttribute("y1", map1.get(crc[i]).getAttribute("cy"));
-            // curr = ++curr % map1.size;
-            // lne[i].setAttribute("x2", map1.get(crc[curr]).getAttribute("cx"));
-            // lne[i].setAttribute("y2", map1.get(crc[curr]).getAttribute("cy"));
+        let key = crcCord.keys();
+        let val = crcCord.values();
+        for (let i=0;i<crcCord.size;i++) {
+            pnta.attributes.d.value += `${key.next().value}, ${val.next().value} `;
         };
         pnta.attributes.d.value += "z";
-        await sleep(30);
+        await sleep(20);
     };
-    function coordinateTransform(screenPoint, someSvgObject) {
-        var CTM = someSvgObject.getScreenCTM();
-        return screenPoint.matrixTransform( CTM.inverse() );
-    };
-    // one.cx.baseVal.value;
-    // one.cy.baseVal.value;
-    // one.lastElementChild;
-    // let m = one.getTransformToElement(saas);
-    // let asdsa = one.getAttributeNS(null, 'cx');
-    // let onePos = one.getBoundingClientRect();
-    // let bboxGroup = one.getBBox();
-    // let a = one.getScreenCTM();
-    // let s = one.getCTM();
-    // let inverse = a.inverse();
-    // let inverse = a.getScreenCoords();
-    // var CTM = one.getScreenCTM();
-    // line.x1.baseVal.value = (one.clientLeft - CTM.e) / CTM.a;
-    // line.y1.baseVal.value = (one.clientTop - CTM.f) / CTM.d;
-    // line.x1.baseVal.value = s.e + 170;
-    // line.y1.baseVal.value = -s.f;
-    // line.x1.baseVal.value = -inverse.e;
-    // line.y1.baseVal.value = -s.f;
-    // line.x1.baseVal.value = one.cx.baseVal.value;
-    // line.y1.baseVal.value = one.cy.baseVal.value;
-    // line.x2.baseVal.value;
-    // line.y2.baseVal.value;
-    // let das = one._getAbsoluteCTM();
 });
-
-
-// let flag = 0;
-// // svg = document.querySelector('#ONE');
-// let losta = document.querySelector('#losta');
-// // $svg = $(svg);
-
-// let pause = function() {
-//     let fasd = losta.animate();
-//     // fasd.progress();
-//     // let rect = draw.rect(100, 100);
-//     // let runner = rect.animate();
-//     losta.progress();
-// };
-
-
-// function distance(start: Point, end: Point) {
-//     const dx = to.x - from.x
-//     const dy = to.y - from.y
-
-//     return Math.sqrt(dx * dx + dy * dy)
-// }
-
-// function calculatePath(start: Point, end: Point) {
-//     const center = {
-//         x: (start.x + end.x) / 2,
-//         y: (start.y + end.y) / 2,
-//     }
-
-//     const controlPoint = {
-//         x: start.x + Math.min(
-//             distance(start, end),
-//             Math.abs(end.y - start.y) / 2,
-//             150
-//         ),
-//         y: start.y,
-//     }
-
-//     return `M ${start.x},${start.y} Q ${controlPoint.x}, ${controlPoint.y} ${center.x},${center.y} T ${end.x},${end.y}`
-// }
-
-
-
 
 
 
 
 
 //Анимация скиллов
+let parametr = document.querySelector(".parametr");
+let states = document.querySelectorAll(".state");
+
+const mapCoupleCP = new Map([
+    [/64% 65%/g, '67% 70.62%'],
+    [/67% 70.62%/g, '64% 65%'],
+    [/66% 69%/g, '68% 73%'],
+    [/68% 73%/g, '66% 69%'],
+
+
+    [/75.94% 37.5%/g, '78% 36.5%'],
+    [/78% 36.5%/g, '75.94% 37.5%'],
+    [/73.8% 38%/g, '82% 35.6%'],
+    [/82% 35.6%/g, '73.8% 38%'],
+
+
+    [/49.9% 17.12%/g, '49.9% 12%'],
+    [/49.9% 12%/g, '49.9% 17.12%'],
+    [/49.8% 21.25%/g, '49.8% 10.75%'],
+    [/49.8% 10.75%/g, '49.8% 21.25%'],
+
+
+    [/22% 36.8%/g, '17% 35%'],
+    [/17% 35%/g, '22% 36.8%'],
+    [/24.69% 37.8%/g, '17.56% 35.4%'],
+    [/17.56% 35.4%/g, '24.69% 37.8%'],
+    
+
+    [/34% 68%/g, '31.5% 72%'],
+    [/31.5% 72%/g, '34% 68%'],
+    [/35.59% 66.24%/g, '31.4% 72.23%'],
+    [/31.4% 72.23%/g, '35.59% 66.24%'],
+]);
 document.addEventListener('DOMContentLoaded', async function(e) {
-    R = {
-        '66% 68%':'65% 67.25%',
-        '67% 70.62%':'64% 65%',
-        '65% 67.25%':'66% 68%',
-        '64% 65%':'67% 70.62%',
-        
-
-
-        // '73.8% 38%':'82% 35.6%',
-        // '75.94% 37.5%':'',
-        // '82% 35.6%':'73.8% 38%',
-        // '':'75.94% 37.5%',
-
-
-        // '':'',
-        // '':'',
-    };
-    // for( from in R) key = key.replace( from, R[ from]);
     while(parametr) {
-        // parametr.style.clipPath = parametr.style.clipPath.replace(/64% 65%/g, '67% 70.62%');
-        for(from in R) parametr.style.clipPath = parametr.style.clipPath.replace( from, R[from]);
-        // parametr.style.clipPath = parametr.style.clipPath.replace(/5/g, '65% 67.25%');
-        await sleep(100);
-        parametr.style.clipPath = parametr.style.clipPath.replace(/67% 70.62%/g, '64% 65%');
-        parametr.style.clipPath = parametr.style.clipPath.replace(/73.8% 38%/g, '82% 35.6%');
-        await sleep(100);
-        parametr.style.clipPath = parametr.style.clipPath.replace(/82% 35.6%/g, '73.8% 38%');
-        parametr.style.clipPath = parametr.style.clipPath.replace(/49.8% 21.25%/g, '49.8% 10.75%');
-        await sleep(100);
-        parametr.style.clipPath = parametr.style.clipPath.replace(/49.8% 10.75%/g, '49.8% 21.25%');
-        parametr.style.clipPath = parametr.style.clipPath.replace(/24.69% 37.8%/g, '17.56% 35.4%');
-        await sleep(100);
-        parametr.style.clipPath = parametr.style.clipPath.replace(/17.56% 35.4%/g, '24.69% 37.8%');
-        parametr.style.clipPath = parametr.style.clipPath.replace(/35.59% 66.24%/g, '31.4% 72.23%');
-        await sleep(100);
-        parametr.style.clipPath = parametr.style.clipPath.replace(/31.4% 72.23%/g, '35.59% 66.24%');
+        for (const [key, value] of mapCoupleCP.entries()) {
+            if (parametr.style.clipPath.search(key) > 0) {
+                parametr.style.clipPath = parametr.style.clipPath.replace(key, value);
+                await sleep(100);
+            }
+        }
         await sleep(3000);
     }
+});
+
+//Выбор скилла
+document.addEventListener('mouseover', function(e) {
+    if (e.target.className.baseVal != "state") return;
+
+    if (e.target == states[0]) {
+        parametr.style.clipPath = parametr.style.clipPath.replace(/49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%/g, '49.9% 17.12%, 22.2% 4.5%, 22% 36.8%');
+    } else if (e.target == states[1]) {
+        parametr.style.clipPath = parametr.style.clipPath.replace(/24.69% 37.8%, 10.73% 58.3%, 35.59% 66.24%/g, '22% 36.8%, 4.8% 60.3%, 34% 68%');
+    } else if (e.target == states[2]) {
+        parametr.style.clipPath = parametr.style.clipPath.replace(/49.8% 75.51%, 64% 65%/g, '49.9% 94.7%, 66% 69%').replace(/35.59% 66.24%/g, '34% 68%');
+    } else if (e.target == states[3]) {
+        parametr.style.clipPath = parametr.style.clipPath.replace(/64% 65%, 91.2% 59%, 73.8% 38%/g, '66% 69%, 95% 60.3%, 75.94% 37.5%');
+    } else if (e.target == states[4]) {
+        parametr.style.clipPath = parametr.style.clipPath.replace(/73.8% 38%, 67.68% 20.58%, 49.8% 21.25%/g, '75.94% 37.5%, 77.68% 4.5%, 49.9% 17.12%');
+    }
+});
+
+document.addEventListener('mouseout', function(e) {
+    if (e.target.classList[0] != "state") return;
+    parametr.style.clipPath = "polygon(49.8% 75.51%, 64% 65%, 91.2% 59%, 73.8% 38%, 67.68% 20.58%, 49.8% 21.25%, 29.2% 16.45%, 24.69% 37.8%, 10.73% 58.3%, 35.59% 66.24%)"
 });
 
 //Выбор категории
