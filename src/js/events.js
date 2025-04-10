19153d738581963b48a8eeeda60547041571bae3
Danila Dippel <erroraeto@gmail.com>
2025-04-10 22:56:42 +0300

    gulp install correct

--- src/js/events.js
+++ src/js/events.js
@@ -52,54 +52,54 @@ function sleep(ms) {
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 //Свайпы секций
-let main = document.querySelector('main')
+// let main = document.querySelector('main')
 
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
+// document.addEventListener('touchstart', handleTouchStart, false);
+// document.addEventListener('touchmove', handleTouchMove, false);
 
-let xDown = null;
-let yDown = null;
+// let xDown = null;
+// let yDown = null;
 
-function handleTouchStart(evt) {
-    xDown = evt.touches[0].clientX;
-    yDown = evt.touches[0].clientY;
-    main.children;
-    main.children.length;
-};
+// function handleTouchStart(evt) {
+//     xDown = evt.touches[0].clientX;
+//     yDown = evt.touches[0].clientY;
+//     main.children;
+//     main.children.length;
+// };
 
-let tochScrollPos = 2;
+// let tochScrollPos = 2;
 
-function handleTouchMove(evt) {
-    // if ( evt.target.closest('#nCM') ) return;
-    if ( ! xDown || ! yDown ) return;
+// function handleTouchMove(evt) {
+//     // if ( evt.target.closest('#nCM') ) return;
+//     if ( ! xDown || ! yDown ) return;
 
-    let xUp = evt.touches[0].clientX;
-    let yUp = evt.touches[0].clientY;
+//     let xUp = evt.touches[0].clientX;
+//     let yUp = evt.touches[0].clientY;
 
-    let xDiff = xDown - xUp;
-    let yDiff = yDown - yUp;
+//     let xDiff = xDown - xUp;
+//     let yDiff = yDown - yUp;
 
-    // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) {
-            /* left swipe */
-            if (--main.children.length > tochScrollPos) ++tochScrollPos;
-        } else {
-            /* right swipe */
-            if (0 < tochScrollPos) --tochScrollPos;
-        }
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
-        } else { 
-            /* down swipe */
-        }
-    }
-    xDown = null;
-    yDown = null;
-    console.log(tochScrollPos);
-    main.children[tochScrollPos].scrollIntoView({ behavior: 'smooth' });
-};
+//     // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+//         if ( xDiff > 0 ) {
+//             /* left swipe */
+//             if (--main.children.length > tochScrollPos) ++tochScrollPos;
+//         } else {
+//             /* right swipe */
+//             if (0 < tochScrollPos) --tochScrollPos;
+//         }
+//     } else {
+//         if ( yDiff > 0 ) {
+//             /* up swipe */ 
+//         } else { 
+//             /* down swipe */
+//         }
+//     }
+//     xDown = null;
+//     yDown = null;
+//     console.log(tochScrollPos);
+//     main.children[tochScrollPos].scrollIntoView({ behavior: 'smooth' });
+// };
 
 //Скрытие доступного текста для скролла
 let scrollable = document.querySelectorAll(".scrollable");
